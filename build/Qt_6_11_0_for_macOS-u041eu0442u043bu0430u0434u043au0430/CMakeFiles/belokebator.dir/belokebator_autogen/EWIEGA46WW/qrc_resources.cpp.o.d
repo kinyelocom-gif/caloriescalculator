78fbecaf965e8c944e7c0ddf6f30ad6aaaac9eb9
Mykola <kinyelocom@gmail.com>
2026-04-03 18:33:25 +0300

    Clean up project structure and fix gitignore

--- build/Qt_6_11_0_for_macOS-u041eu0442u043bu0430u0434u043au0430/CMakeFiles/belokebator.dir/belokebator_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/belokebator.dir/belokebator_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
-  /Users/nikolay/Documents/алгоротмизация/курсова/belokebator/build/Qt_6_11_0_for_macOS-u041eu0442u043bu0430u0434u043au0430/belokebator_autogen/EWIEGA46WW/qrc_resources.cpp
